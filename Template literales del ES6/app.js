658380955ecde1e42b82450a6ecd93992a9087ce
danielhuaman <danielsam_m@hotmail.com>
2023-12-25 06:44:09 -0500

    Nueva clase

--- Template literales del ES6/app.js
+++ Template literales del ES6/app.js
@@ -1,10 +1,13 @@
-'use strict'
-;(function () {
-  const nombre = 'Daniel'
-  const apellido = 'Huaman'
-  const edad = 23
-  // const salida = nombre + apellido + edad:
-  const salida = nombre + '' + apellido + ' ( ' + edad + ' ) '
-  //Daniel Huaman  (Edad : 23)
-  console.log(salida)
-})()
+(function () {
+    function getEdad() {
+        return 100 + 100 + 300;
+    }
+    var nombre = 'Daniel';
+    var apellido = 'Huaman';
+    var edad = 23;
+    // const salida = nombre + apellido + edad:
+    // const salida = nombre + "" + apellido + "(" + edad + " ) ";
+    var salida = "".concat(nombre, " ").concat(apellido, " ( ").concat(getEdad(), " )");
+    //Daniel Huaman  (Edad : 23)
+    console.log(salida);
+})();
--- Template literales del ES6/app.ts
+++ Template literales del ES6/app.ts
@@ -1,10 +1,16 @@
 (function () {
+
+    function getEdad() {
+        return 100 + 100 + 300;
+    }
+
     const nombre = 'Daniel';
     const apellido = 'Huaman';
     const edad = 23;
 
     // const salida = nombre + apellido + edad:
-    const salida = nombre + "" + apellido + "(" + edad + " ) ";
+    // const salida = nombre + "" + apellido + "(" + edad + " ) ";
+    const salida = `${ nombre } ${ apellido } ( ${ getEdad() } )`;
     //Daniel Huaman  (Edad : 23)
     console.log(salida);
 
