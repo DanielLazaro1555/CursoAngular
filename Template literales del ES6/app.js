7243b6004e5b3100729c40ac2bc3700272d340bc
danielhuaman <danielsam_m@hotmail.com>
2023-12-25 07:06:19 -0500

    Nueva clase

--- Template literales del ES6/app.js
+++ Template literales del ES6/app.js
@@ -1,13 +1,14 @@
+"use strict";
 (function () {
     function getEdad() {
         return 100 + 100 + 300;
     }
-    var nombre = 'Daniel';
-    var apellido = 'Huaman';
-    var edad = 23;
+    const nombre = 'Daniel';
+    const apellido = 'Huaman';
+    const edad = 23;
     // const salida = nombre + apellido + edad:
     // const salida = nombre + "" + apellido + "(" + edad + " ) ";
-    var salida = "".concat(nombre, " ").concat(apellido, " ( ").concat(getEdad(), " )");
+    const salida = `${nombre} ${apellido} ( ${getEdad()} )`;
     //Daniel Huaman  (Edad : 23)
     console.log(salida);
 })();
